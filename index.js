24b87133f1f58655acaaadf201faa39f088f5ee5
ezfahmi <eikzlfkr@gmail.com>
2021-04-17 21:37:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const hostname = '127.0.0.1'
 const port = 3000
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
+  res.statusCode = 20;
   res.setHeader('Content-Type', 'text/plain')
   res.end('Server is running...')
 })
